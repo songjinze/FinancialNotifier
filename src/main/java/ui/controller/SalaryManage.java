17723439c3b415380e92f60e2e2e320493fcb160
songjinze <734609160@qq.com>
2018-08-24 18:37:35 +0800

    添加业务逻辑单元测试，管理薪酬界面

---
+++ src/main/java/ui/controller/SalaryManage.java
@@ -0,0 +1,30 @@
+package ui.controller;
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+
+
+public class SalaryManage {
+
+    @FXML private TableView employeeList;
+    @FXML private TableColumn employeeIdColumn;
+    @FXML private TableColumn employeeNameColumn;
+    @FXML private TableColumn employeeEmailColumn;
+    @FXML private TableColumn employeeSalaryColumn;
+    @FXML private Button searchButton;
+    @FXML private TextField searchField;
+
+    private SalaryManage(){}
+    private static volatile SalaryManage salaryManage;
+    public static SalaryManage getInstance(){
+        if(salaryManage==null){
+            synchronized (SalaryManage.class) {
+                salaryManage = new SalaryManage();
+            }
+        }
+        return salaryManage;
+    }
+
+    
+
+
+}
